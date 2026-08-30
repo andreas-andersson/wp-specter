bec76f3978e7f5134229be18cb6a428842b8bc59
Andreas Andersson <andreas.1.andersson@gavle.se>
2026-08-30 17:36:43 +0200

    fix: update internal version

--- src/Application.php
+++ src/Application.php
@@ -27,7 +27,7 @@ use WpSpecter\Support\GlobExpander;
 
 class Application
 {
-    private const VERSION = '0.5.0';
+    private const VERSION = '0.6.0';
 
     /** @param list<string> $argv */
     public function run(array $argv): int
